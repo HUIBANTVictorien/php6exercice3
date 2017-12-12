0c73c2d79a85a21dd35be6bbc59b1a72c9534315
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 13:51:56 +0100

    ajout exo3

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<?php
-$numberOne = 100;
-$numberTwo = 6;
-while ($numberOne >=10) {
-  echo ($numberTwo * $numberOne.' ');
-  $numberOne --;
-}
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 3 partie 4 php</title>
+  </head>
+  <body>
+    <?php
+    function returnStringConcat() {
+      $text1 = 'Aller salut et merci bien pour le poisson';
+      $text2 = '<br/>H2G2 !';
+      return $text1.$text2;
+    }
+    echo returnStringConcat();
+    ?>
+  </body>
+</html>
